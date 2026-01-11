111342c0ddca506d37e6ef0e65298a286e167ac0
Aleks Zimek <zimek.aleks@gmail.com>
2026-01-11 22:11:54 +0100

    Dodanie komponentu MainHeaderBackgroud z SVG gradient

--- app/meals/[someName]/page.js
+++ app/meals/[someName]/page.js
@@ -1,9 +1,11 @@
-export default function MealDetailsPage({ params }) {
+export default async function MealDetailsPage({ params }) {
+    const { someName } = await params;
+    
     return (
         <main>
             <h1 style={{ color: 'white', textAlign: 'center' }}>Meal Details</h1>
             <p style={{ color: '#ddd', textAlign: 'center' }}>
-                Dynamiczny segment: <b>{params.someName}</b>
+                Dynamiczny segment URL: <b>{someName}</b>
             </p>
         </main>
     );
--- components/main-header.js
+++ components/main-header.js
@@ -1,28 +1,43 @@
 import Link from 'next/link';
+import Image from 'next/image';
+
 import logoImg from '@/assets/logo.png';
+import mealIcon from '@/assets/icons/meal.png';
+import communityIcon from '@/assets/icons/community.png';
+import eventsIcon from '@/assets/icons/events.png';
+
 import classes from './main-header.module.css';
 
 export default function MainHeader() {
-    return (
-        <header className={classes.header}>
-            <Link className={classes.logo} href='/'>
-            <img src={logoImg.src} alt="Foodies logo" />
-            NextLevel Food
-            </Link>
+  return (
+    <header className={classes.header}>
+      <Link className={classes.logo} href="/">
+        <Image src={logoImg} alt="Foodies logo" priority />
+        NextLevel Food
+      </Link>
 
-            <nav> className={classes.nav}>
-                <ul>
-                    <li>
-                        <Link href="/meals">Meals</Link>
-                    </li>
-                    <li>
-                        <Link href="/community">Community</Link>
-                    </li>
-                    <li>
-                        <Link href="/meals/share">Share</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <Link href="/meals">
+              <img src={mealIcon.src} alt="" />
+              Meals
+            </Link> 
+          </li>
+          <li>
+            <Link href="/community">
+              <img src={communityIcon.src} alt="" />
+              Community
+            </Link>
+          </li>
+          <li>
+            <Link href="/meals/share">
+              <img src={eventsIcon.src} alt="" />
+              Share
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
