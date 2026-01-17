30bde76bb7bd7fe3a552aab9890b6f7ba5abfc13
Aleks Zimek <zimek.aleks@gmail.com>
2026-01-17 11:14:21 +0100

    Zmiana nazwy dynamicznego routingu, wykonanie poleceń z całego labolatorium.

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,11 +1,11 @@
 export default async function MealDetailsPage({ params }) {
-    const { someName } = await params;
+    const { slug } = await params;
     
     return (
         <main>
             <h1 style={{ color: 'white', textAlign: 'center' }}>Meal Details</h1>
             <p style={{ color: '#ddd', textAlign: 'center' }}>
-                Dynamiczny segment URL: <b>{someName}</b>
+                Dynamiczny segment URL: <b>{slug}</b>
             </p>
         </main>
     );
--- components/main-header.js
+++ components/main-header.js
@@ -2,9 +2,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 import logoImg from '@/assets/logo.png';
-import mealIcon from '@/assets/icons/meal.png';
-import communityIcon from '@/assets/icons/community.png';
-import eventsIcon from '@/assets/icons/events.png';
+import NavLink from './nav-link';
 
 import classes from './main-header.module.css';
 
@@ -19,22 +17,10 @@ export default function MainHeader() {
       <nav className={classes.nav}>
         <ul>
           <li>
-            <Link href="/meals">
-              <img src={mealIcon.src} alt="" />
-              Meals
-            </Link> 
+            <NavLink href="/meals">Browse Meals</NavLink>
           </li>
           <li>
-            <Link href="/community">
-              <img src={communityIcon.src} alt="" />
-              Community
-            </Link>
-          </li>
-          <li>
-            <Link href="/meals/share">
-              <img src={eventsIcon.src} alt="" />
-              Share
-            </Link>
+            <NavLink href="/community">Foodies Community</NavLink>
           </li>
         </ul>
       </nav>
